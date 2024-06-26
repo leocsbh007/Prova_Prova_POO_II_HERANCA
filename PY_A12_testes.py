624d284523a1f2b8ff1939f7b9b0e5832243db90
Leonardo Souza <leocsbh007@yahoo.com.br>
2024-06-26 15:21:57 -0300

    Create PY_A12_testes.py
    Teste da Class Material e suas filhas Livro e Revista

---
+++ PY_A12_testes.py
@@ -0,0 +1,7 @@
+from PY_A12 import Material, Livro, Revista
+
+revista= Revista('Bicicleta Amarela', 'Editora Classica', '3ª Edição')
+livro = Livro('A volta dos que não foram', 'Editora Viagem', 'Comedia')
+
+revista.exibir_informacoes()
+livro.exibir_informacoes()
